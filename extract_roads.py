ea0268b77b6478bec269a981ada197025a99e50a
Jean-Francois Bourgon <jean-francois.bourgon@canada.ca>
2024-11-04 19:46:54 +0000

    Revert "linux version for osgeo/grass-gis:releasebranch_8_4-ubuntu"
    This reverts commit 34159cc5f9f905dfde739d79ad13ec48deff9870.

--- extract_roads.py
+++ extract_roads.py
@@ -1,72 +1,151 @@
 import os
 import gc
+import csv
+import sys
 import time
+import shutil
 import tempfile
 import argparse
-import uuid
-#import rasterio
+import binascii
+import subprocess
 from pathlib import Path
 from osgeo import gdal, osr
 
-# Import GRASS Python bindings
-from grass_session import Session
-import grass.script as grass
-import grass.script.setup as gsetup
 
-#gdal.DontUseExceptions()
-MAPSET="PERMANENT"
+gdal.DontUseExceptions()
 
 def cmd_interface(argv=None):
     
     parser = argparse.ArgumentParser(
         usage="%(prog)s [-h HELP] use -h to get supported arguments.",
-        description="Extract roads from a raster mask.",
+        description="Extract roads from raster masks.",
     )
-    parser.add_argument("-i", "--input", help="The path to the input road raster masks")
-    parser.add_argument("-o", "--output", help="The path to the output road vector file")
+    parser.add_argument("-i", "--input_csv", nargs=1, help="A csv of all road raster masks")
+    parser.add_argument("-wd", "--work_dir", nargs=1, help="The dir for saving geopackages")
     
     args = parser.parse_args()
+    input_csv = args.input_csv[0]
+    work_dir = args.work_dir[0] 
+
     arguments = {
-        "input": args.input,
-        "output": args.output,
+        "input_csv": input_csv,
+        "work_dir": work_dir,
     }
     return arguments
-   
-if __name__ == "__main__":
-    arguments = cmd_interface()
 
-    # Creating the geofile path for storing the results
-    road_dataset_path = Path(arguments["input"])
-    road_dataset_name = road_dataset_path.stem
-    road_geopackage = Path(arguments["output"]) / f"{road_dataset_name}.gpkg"
-
-    # Getting the crs code of the layer
-    road_dataset = gdal.Open(road_dataset_path)
+def config_grass():
     
-    epsg=""
-    if road_dataset is not None:
-        # Get the CRS from the dataset
-        projection = road_dataset.GetProjection()
-
-        # Parse the projection information to get the EPSG code
-        srs = osr.SpatialReference(wkt=projection)
-        srs.AutoIdentifyEPSG()
-        epsg = srs.GetAuthorityCode(None)   
-
-    if epsg:
-        start_time = time.time()
+    
+    result = subprocess.run(['where', 'grass84.bat'], capture_output=True, text=True, check=True)
         
-        with tempfile.TemporaryDirectory() as gisdb:
-            with Session(gisdb=gisdb, 
-                         location=str(uuid.uuid4()), 
-                         create_opts= f"EPSG:{epsg}"):                
-                # Print current GRASS GIS environment
-                print("--- GRASS GIS - Current GRASS GIS environment ---")
-                print(f"GRASS Version {grass.version().version}")
-                print(grass.gisenv())
+    # Output will contain the path to grass84.bat
+    grass8bin = grass8bin_win = result.stdout.strip()
+    
+    if grass8bin:
+        print(f"Path to grass84 bat: {grass8bin}")
+    else:
+        print("grass84.bat not found.")
+
+    """ The following command is for checking if grass has its start script"""
+    startcmd = grass8bin + ' --config path'
+    p = subprocess.Popen(startcmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if p.returncode != 0:
+        print(f'ERROR: {err.decode()}', file=sys.stderr)
+        print(f'ERROR: Cannot find GRASS GIS 7 start script ({startcmd})', file=sys.stderr)
+        sys.exit(-1)
+
+    """ Now setting GISBASE"""
+    if b"OSGEO4W home is" in out:
+        gisbase = out.strip().decode().split('\n')[1]
+    else:
+        gisbase = out.strip().decode()
+
+    os.environ['GRASS_SH'] = os.path.join(gisbase, 'msys', 'bin', 'sh.exe')
+    os.environ['GISBASE'] = gisbase
+
+    """ Now setting GRASS Python path"""
+    gpydir = os.path.join(gisbase, "etc", "python")
+    sys.path.append(os.environ.get('PYTHONPATH', ''))
+    sys.path.append(gpydir)
+    
+    return grass8bin
+
+    
+if __name__ == "__main__":
+
+    
+
+    arguments = cmd_interface()
+    grass8bin = grass8bin_win = config_grass()
+    """ define GRASS DATABASE """
+    gisdb = os.path.join(os.getenv('APPDATA', 'grassdata'))
+    gisdb = os.path.join(tempfile.gettempdir(), 'grassdata')
+    try:
+        os.stat(gisdb)
+    except:
+        os.mkdir(gisdb)
+
+    with open(arguments["input_csv"], newline='',encoding='utf-8-sig') as csvfile:
+        reader = csv.reader(csvfile)
+        for road_mask in reader:
+            # Creating the geofile path for storing the results
+            road_dataset_name = os.path.splitext(os.path.basename(Path(road_mask[0])))[0]
+            road_geopackage = Path(arguments["work_dir"]) / f"{road_dataset_name}.gpkg"
+            # Getting the crs code of the layer
+            road_dataset = gdal.Open(road_mask[0])
+            location_epsg=""
+            if road_dataset is not None:
+                # Get the CRS from the dataset
+                spatial_ref = osr.SpatialReference()
+                spatial_ref.ImportFromWkt(road_dataset.GetProjectionRef())
+                location_epsg = spatial_ref.GetAttrValue("AUTHORITY", 1)
+                del spatial_ref
+            del road_dataset
+            if len(location_epsg) != 0:
+
+                start_time = time.time()
+                """ location/mapset: use random names """
+                location = binascii.hexlify(os.urandom(8))
+                mapset='PERMANENT'
+                if isinstance(gisdb, bytes):
+                    gisdb = gisdb.decode('utf-8')
+                if isinstance(location, bytes):
+                    location = location.decode('utf-8')
+
+                # Create new location from EPSG code
+                location_path = os.path.join(gisdb, location)
+                startcmd = grass8bin + ' -c epsg:' + location_epsg + ' -e ' + location_path
+                p = subprocess.Popen(startcmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                out, err = p.communicate()
+
+                if p.returncode != 0:
+                    print(f'ERROR: {err.decode()}', file=sys.stderr)
+                    print(f'ERROR: Cannot generate location ({startcmd})', file=sys.stderr)
+                    sys.exit(-1)
+                else:
+                    print(f'Created location {location_path}')
                 
+                ########
+                """ Now the location with PERMANENT mapset exists."""
+                os.environ['GISDBASE'] = gisdb
+                os.environ['LANG'] = 'en_US'
+                os.environ['LOCALE'] = 'C'
+
+
+                # Import GRASS Python bindings
+                import grass.script as grass
+                import grass.script.setup as gsetup
+
+                gsetup.init(gisdb,location, mapset)
+                print(grass.read_command('g.gisenv', flags='s'))
+                # Print current GRASS GIS environment
+                grass.message('--- GRASS GIS 7: Current GRASS GIS 7 environment:')
+                print(grass.gisenv())
                 output_name = "roads"
-                grass.run_command('r.import', input=road_dataset_path, output=output_name, overwrite=True)
+                #sys.path.append(subprocess.check_output([grass8bin, "--config", "python_path"], text=True, shell=True).strip())
+                
+                grass.run_command('r.import', input=road_mask, output=output_name, overwrite=True)
                 grass.run_command('g.region', flags='p', raster=output_name)
                 grass.run_command('r.to.vect', input=output_name, output='roads_v', type='line', flags='s')
                 grass.run_command('v.generalize', input='roads_v', output='roads_v_smooth', method='chaiken', threshold=10)
@@ -80,8 +159,10 @@ if __name__ == "__main__":
                 
                 total_time = time.time() - start_time
                 print(
-                    f"------------- Road Extraction for {road_dataset_name} Completed in {(total_time // 60):.0f}m {(total_time % 60):.0f}s -------------"
+                    f"--------------------------- Road Extraction for {road_dataset_name} Completed in {:.0f}m {:.0f}s -------------------------------".format(
+                        total_time // 60, total_time % 60
+                    )
                 )
-                #shutil.rmtree(location_path)
+                shutil.rmtree(location_path)
                 gc.collect()
 
